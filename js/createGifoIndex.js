10672f93f67de577a6d208ba350d0e5a6d826c89
darodweb <darodweb@gmail.com>
2021-04-25 01:00:02 -0300

    Styles update to Create Gif page

--- js/createGifoIndex.js
+++ js/createGifoIndex.js
@@ -5,11 +5,61 @@ import {
     FAVORITES_CONTAINER, URL_SEARCH_BY_ID, CREATE_GIF_TITLE, CREATE_GIF_MESSAGE, START_BUTTON,
     RECORD_BUTTON, RECORD_VIDEO, CANVAS_CONTAINER, STEP_1, STEP_2, STEP_3, STOP_BUTTON, UPLOAD_BUTTON,
     TIMER, REPEAT_CAPTION, UPLOAD_URL, RESET_BUTTON, RECORDING_VIDEO_CONTAINER, GIF_ID_URL,
-    LABEL_API_KEY, API_KEY, UPLOAD_OVERLAY, UPLOAD_LOADER, UPLOAD_OK, UPLOAD_MESSAGE, GIF_BY_ID_URL
+    LABEL_API_KEY, API_KEY, UPLOAD_OVERLAY, UPLOAD_LOADER, UPLOAD_OK, UPLOAD_MESSAGE, GIF_BY_ID_URL,
+    BODY, DARK_MODE_MENU, LOGO_DARK_MODE, LOGO_DARK_LIGHT_MODE, DARK_MODE_DESKTOP_MENU
 } from './constants.js';
 import { recordRTC } from './recordRTC.js'
 
-//Hamburger
+//-------DARK MODE TOGGLE--------------------------
+function changeMobileMenuText(event) {
+    let _innerText = event.target.textContent;
+    if (_innerText === "Modo Nocturno") {
+        DARK_MODE_MENU.innerHTML = "Modo Diurno";
+        LOGO_DARK_MODE.classList.remove('hidden');
+        LOGO_DARK_LIGHT_MODE.classList.add('hidden');
+    } else if (_innerText === "Modo Diurno") {
+        DARK_MODE_MENU.innerHTML = "Modo Nocturno";
+        LOGO_DARK_MODE.classList.add('hidden');
+        LOGO_DARK_LIGHT_MODE.classList.remove('hidden');
+    }
+}
+
+function changeDesktopMenuText(event) {
+    let _innerTextDesktop = event.target.textContent;
+    console.log(_innerTextDesktop);
+    if (_innerTextDesktop === "MODO NOCTURNO") {
+        DARK_MODE_DESKTOP_MENU.innerHTML = "MODO DIURNO";
+        LOGO_DARK_MODE.classList.remove('hidden');
+        LOGO_DARK_LIGHT_MODE.classList.add('hidden');
+    } else {
+        DARK_MODE_DESKTOP_MENU.innerHTML = "MODO NOCTURNO";
+        LOGO_DARK_MODE.classList.add('hidden');
+        LOGO_DARK_LIGHT_MODE.classList.remove('hidden');
+    }
+}
+
+//For desktop
+DARK_MODE_DESKTOP_MENU.addEventListener('click', (event) => {
+    let currentTheme = localStorage.setItem('theme', 'dark')
+    BODY.classList.toggle('dark-mode');
+    changeDesktopMenuText(event);
+})
+
+
+//FOr Mobile
+DARK_MODE_TRIGGER.addEventListener('click', (event) => {
+    let currentTheme = localStorage.setItem('theme', 'dark')
+    BODY.classList.toggle('dark-mode');
+    changeMobileMenuText(event);
+})
+
+DAY_MODE_MENU.addEventListener('click', (event) => {
+    BODY.classList.toggle('dark-mode');
+    changeMobileMenuText(event);
+
+})
+
+//-----------HAMBURGER MENU-----------------------
 let burgerCloseButtonState = false;
 
 hamburger.addEventListener('click', () => {
@@ -18,10 +68,10 @@ hamburger.addEventListener('click', () => {
     menu.classList.toggle('display');
 
     if (burgerCloseButtonState !== false) {
-        hamburger.style.backgroundImage = `url('../assets/close.svg')`;
+        hamburger.style.backgroundImage = `var(--close-hamburger)`;
 
     } else if (burgerCloseButtonState === false) {
-        hamburger.style.backgroundImage = `url('../assets/burger.svg')`;
+        hamburger.style.backgroundImage = `var(--hamburger)`;
     }
 
 })
